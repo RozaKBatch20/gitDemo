e690c3dc3b71339f817af0210c891232d65da8ff
RozaKBatch20 <zozik.m@gmail.com>
2020-07-29 21:54:48 -0400

    first commit

---
+++ src/GitIntro.java
@@ -0,0 +1,8 @@
+public class GitIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello B20, it is my pleasure to be with you ");
+
+        // added line
+    }
+}
