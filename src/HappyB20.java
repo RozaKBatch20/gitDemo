7a9d1f700e4d3787396eebd835f286e4f86dfb1b
Roza Kydykeev <68828831+RozaKBatch20@users.noreply.github.com>
2020-08-01 16:33:48 -0400

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,6 +3,8 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
+        
+        // added new lines
 
         System.out.println("I'm excited to start my carrer as SDET");
     }
